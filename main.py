8b79f5a4f332ecfdda0da0a9ecf564d76084a57d
Walker Laury <walkerclaury@protonmail.com>
2019-05-06 11:28:01 -0500

    finally all working

---
+++ main.py
@@ -0,0 +1,171 @@
+from flask import Flask, request, redirect, render_template, session
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:localhost@localhost:3306/blogz'
+app.config['SQLALCHEMY_ECHO'] = True
+app.secret_key = 'wakaflaka123'
+db = SQLAlchemy(app)
+#Connects to the database
+@app.before_request
+def require_login():
+    allowed_routes = ['index', 'blog', 'login', 'signup']
+    if request.endpoint not in allowed_routes and 'email' not in session:
+        return redirect('/login')
+
+
+#Create a class for each blog post with Id, title, Body
+class Blog(db.Model):
+    
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(500))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __init__(self, title, body, owner):
+        self.title = title
+        self.body = body
+        self.owner = owner
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+    blogs = db.relationship('Blog', backref='owner')
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+
+
+@app.route('/')
+def index():
+    blogs = []
+    id = request.args.get('id')
+    if 'id' in request.args:
+
+        blogs = Blog.query.filter_by(id=id).first()
+        return render_template('post.html', blogs=blogs)
+
+    users = User.query.all()
+    return render_template('index.html', users=users)
+
+@app.route('/blog', methods=['POST', 'GET'])
+def blog():
+    blogs = []
+    if 'user' in request.args:
+        user_id = request.args.getlist('user')
+        user = User.query.get(user_id)
+        user_blogs = Blog.query.filter_by(owner=user).all()
+        return render_template('user.html', user_blogs=user_blogs)
+    
+    blog_id = request.args.get('id')
+    if 'id' in request.args:
+        
+        blogs = Blog.query.filter_by(id=blog_id).all()
+        return render_template('post.html', blogs=blogs)
+    else:
+        blogs = Blog.query.all()
+        return render_template('blog.html', blogs=blogs)
+
+@app.route('/newpost', methods=['POST', 'GET'])
+def new_post():
+
+    title_error = ''
+    text_error = ''
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+
+      
+        if not title:
+            title_error = 'You need to have a title!'
+            return render_template('newpost.html', title_error = title_error, text_error = text_error)
+        if not body:
+            text_error = 'You need to add content!'
+            return render_template('newpost.html', title_error = title_error, text_error = text_error)
+
+        if len(title) > 3 and len(body) > 3:
+            owner = User.query.filter_by(email=session['email']).first()
+            new_entry = Blog(title, body, owner)
+            
+            db.session.add(new_entry)
+            db.session.commit()
+            id = str(new_entry.id)
+            return render_template('post.html', new_entry=new_entry )
+
+
+    return render_template('newpost.html')
+
+
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+    email_error = ''
+    password_error = ''
+    verify_error = ''
+
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        verify = request.form['verify']
+
+        if '@' not in email or ' ' in email or len(email) < 5:
+            email_error = 'not a valid email address'
+        if ' ' in password or len(password) < 3:
+            password_error = 'not a valid password'
+        if password != verify:
+            verify_error = 'Passwords do not match'
+        
+        if email_error or password_error or verify_error:
+            return render_template('signup.html', email = email, email_error=email_error, password_error=password_error, verify_error=verify_error)
+        
+        exist = User.query.filter_by(email=email).first()
+        if exist:
+            email_error = 'That username already exists'
+            return render_template('signup.html', email_error=email_error, password_error=password_error, verify_error=verify_error)
+        
+        if not exist:
+            new_user = User(email, password)
+            db.session.add(new_user)
+            db.session.commit()
+            session['email'] = email
+            return redirect('/newpost')
+    
+
+    return render_template('signup.html')
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    email_error = ''
+    password_error = ''
+    
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email = email).first()
+
+        if user and user.password == password:
+            session['email'] = email
+            return redirect('/newpost')
+
+        if not email:
+            email_error = 'User not found'
+            return render_template('login.html', email_error=email_error, password_error=password_error)
+
+        if user.password != password:
+            password_error = 'Incorrect password'
+            return render_template('login.html', email_error=email_error, password_error=password_error)
+
+    return render_template('login.html', email_error=email_error, password_error=password_error)
+
+
+@app.route('/logout')
+def logout():
+    del session['email']
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run()
